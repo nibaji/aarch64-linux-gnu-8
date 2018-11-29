3289eb8f78113e1e9cc946d8dd90f270dddcd511
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2018-11-30 01:28:41 +0530

    gcc version 8.2.1 20181122
    Signed-off-by: Nidhun Balaji T R <nidhunbalaji@gmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/8.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-8/configure --prefix=/run/media/nibaji/Storage_Big/Development/whyred/toolchains/gcc-8/build_dir/out --target=aarch64-linux-gnu --enable-languages=c,c++ --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
